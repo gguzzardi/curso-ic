21ddcd70df1bbe0e002ddace20dac8623294895d
Gonzalo Guzzardi <gonzalo.guzzardi@mercadolibre.com>
2018-11-29 12:04:21 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 docker stop curso
-docker rm curso
-docker run --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" --link sql-server:sql-server --link some-redis:some-redis -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+docker run --rm --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" --link sql-server:sql-server --link some-redis:some-redis -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
