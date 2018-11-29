5785dc62229455d6b839444051e4f06e44de9e2f
Gonzalo Guzzardi <gonzalo.guzzardi@mercadolibre.com>
2018-11-29 11:04:52 -0300

    Linkea redis

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 docker stop curso
 docker rm curso
-docker run --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" --link sql-server:sql-server -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
+docker run --name curso -e JAVA_OPTS="-Dspring.profiles.active=ic" --link sql-server:sql-server --link some-redis:some-redis -p 8080:8080 -d curso-ic:0.0.1-SNAPSHOT
